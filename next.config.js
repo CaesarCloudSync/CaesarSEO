baf6d1cc05ff7f577f850708961fc18519864334
CaesarCloudSync <113305679+RevisionBank@users.noreply.github.com>
2023-04-24 23:23:31 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/home",
+      destination: "/home.html",
+    },
+  ],
 }
 
 module.exports = nextConfig
